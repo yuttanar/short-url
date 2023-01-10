8ab76f4bf1aaa58229a87d70e7a1c1a3c2a34efe
Yuttanar <yuttanar.tery@gmail.com>
2023-01-10 21:23:27 +0700

    add shortener api

---
+++ pages/[pid].js
@@ -0,0 +1,70 @@
+import { Home } from "@mui/icons-material";
+import { Button, Container, Grid, Typography } from "@mui/material";
+import Head from "next/head";
+import Link from "next/link";
+const redis = require("redis");
+
+export async function getServerSideProps(req) {
+  const { pid } = req.query;
+  let client = redis.createClient({
+    url: process.env.DATABASE_URL,
+  });
+
+  client.on("error", function (err) {
+    throw err;
+  });
+
+  await client.connect();
+
+  const longUrl = await client.get(pid);
+
+  await client.disconnect();
+
+  if (longUrl) {
+    return {
+      redirect: {
+        destination: longUrl,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+const HashPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>URL Shortener</title>
+      </Head>
+      <Container>
+        <Grid
+          container
+          alignContent={"center"}
+          justifyContent="center"
+          style={{
+            minHeight: "100vh",
+          }}
+        >
+          <Grid item sx={12} lg={12} xl={12}>
+            <Typography variant="h3" align="center" component={"div"}>
+              URL not found
+            </Typography>
+          </Grid>
+          <Grid item sx={12} lg={12} xl={12} style={{ textAlign: "center" }}>
+            <Link href="/">
+              <Button variant="contained" startIcon={<Home />}>
+                BACK TO HOME
+              </Button>
+            </Link>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default HashPage;
---
+++ pages/api/short-url.js
@@ -0,0 +1,49 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { customAlphabet } from "nanoid";
+const redis = require("redis");
+
+const urlPattern =
+  /(https:\/\/|http:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      type: "Error",
+      code: 405,
+      message: "Method Not Allowed",
+    });
+  }
+  const { longUrl } = req.body;
+  if (!longUrl || !urlPattern.test(longUrl)) {
+    return res.status(400).send({
+      type: "Error",
+      code: 400,
+      message: "Bad request",
+    });
+  }
+  try {
+    let client = redis.createClient({
+      url: process.env.DATABASE_URL,
+    });
+
+    client.on("error", function (err) {
+      throw err;
+    });
+
+    await client.connect();
+
+    const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789", 5);
+    let urlPath = nanoid();
+    let checkIsExist = await client.get(urlPath);
+
+    while (checkIsExist !== null) {
+      urlPath = nanoid();
+      checkIsExist = await client.get(urlPath);
+    }
+    await client.set(urlPath,longUrl);
+    await client.disconnect();
+    res.status(200).json({ urlPath, longUrl });
+  } catch (error) {
+    res.status(500).json({ type: "Error", code: 500 });
+  }
+}
