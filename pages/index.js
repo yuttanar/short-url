516c76e9bfb278c4732704ebff943e03a3755b9d
Yuttanar <yuttanar.tery@gmail.com>
2023-01-12 23:56:58 +0700

    add url to download ios shortcut

--- pages/index.js
+++ pages/index.js
@@ -261,16 +261,16 @@ export default function Home({text}) {
 
         <div className={styles.grid}>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.icloud.com/shortcuts/e820495791344974a67df4c54a05b611"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
+              IOS <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
+              Dowload IOS Shortcut when share url.
             </p>
           </a>
 
