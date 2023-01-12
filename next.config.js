0b918f5126819f7e5f855dd513bb41c3a9a0149d
Yuttanar <yuttanar.tery@gmail.com>
2023-01-12 23:52:48 +0700

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
+  
 });
