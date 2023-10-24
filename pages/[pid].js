56a5a35ebf54e67d3387e690d59817ff83654a66
Yuttanar <yuttanar.tery@gmail.com>
2023-10-24 09:42:32 +0700

    test

--- pages/[pid].js
+++ pages/[pid].js
@@ -2,6 +2,7 @@ import { Home } from "@mui/icons-material";
 import { Button, Container, Grid, Typography } from "@mui/material";
 import Head from "next/head";
 import Link from "next/link";
+import { useEffect } from "react";
 import { createClient } from "redis";
 
 const redis = createClient({
@@ -16,50 +17,57 @@ export async function getServerSideProps(req) {
   const longUrl = await redis.get(pid);
   await redis.disconnect();
 
-  if (longUrl) {
-    return {
-      redirect: {
-        destination: longUrl,
-        permanent: false,
-      },
-    };
-  }
+  // if (longUrl) {
+  //   return {
+  //     redirect: {
+  //       destination: longUrl,
+  //       permanent: false,
+  //     },
+  //   };
+  // }
 
   return {
-    props: {},
+    props: { longUrl: longUrl ? longUrl : null },
   };
 }
 
-const RedirectPage = () => {
+const RedirectPage = ({ longUrl }) => {
+  useEffect(() => {
+    if (longUrl) {
+      window.location.href = longUrl;
+    }
+  }, []);
   return (
-    <div>
-      <Head>
-        <title>URL Shortener</title>
-      </Head>
-      <Container>
-        <Grid
-          container
-          alignContent={"center"}
-          justifyContent="center"
-          style={{
-            minHeight: "100vh",
-          }}
-        >
-          <Grid item xs={12} lg={12} xl={12}>
-            <Typography variant="h3" align="center" component={"div"}>
-              URL not found
-            </Typography>
+    !longUrl && (
+      <div>
+        <Head>
+          <title>URL Shortener</title>
+        </Head>
+        <Container>
+          <Grid
+            container
+            alignContent={"center"}
+            justifyContent="center"
+            style={{
+              minHeight: "100vh",
+            }}
+          >
+            <Grid item xs={12} lg={12} xl={12}>
+              <Typography variant="h3" align="center" component={"div"}>
+                URL not found
+              </Typography>
+            </Grid>
+            <Grid item xs={12} lg={12} xl={12} style={{ textAlign: "center" }}>
+              <Link href="/">
+                <Button variant="contained" startIcon={<Home />}>
+                  BACK TO HOME
+                </Button>
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item xs={12} lg={12} xl={12} style={{ textAlign: "center" }}>
-            <Link href="/">
-              <Button variant="contained" startIcon={<Home />}>
-                BACK TO HOME
-              </Button>
-            </Link>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+        </Container>
+      </div>
+    )
   );
 };
 
